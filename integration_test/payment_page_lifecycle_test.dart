41748b11289b9503599f0a3508fa9ee9a6b5b33e
Francis Pouliot <francis@satoshiportal.com>
2026-07-07 18:41:55 -0600

    test: freeze the injectable clock in the payment page lifecycle spec

--- integration_test/payment_page_lifecycle_test.dart
+++ integration_test/payment_page_lifecycle_test.dart
@@ -36,6 +36,7 @@ Future<void> main({bool isInitialized = false}) async {
     final bullnym = FakeBullnymClient();
     await wipeAppState(locator);
     await overrideBoundariesForTest(locator, relay: relay, bullnym: bullnym);
+    await overrideClockForTest(locator);
     await locator<CreateDefaultWalletsUsecase>().execute(
       mnemonicWords: getPaidFixtureMnemonicWords,
     );
