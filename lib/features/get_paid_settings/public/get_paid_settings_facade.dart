9d9586a6236c42f4f71d46a162b2e2aa44525a7f
Francis Pouliot <francis@satoshiportal.com>
2026-07-09 19:18:02 -0600

    feat(get-paid): per-product auto-sweep and hide-on-home wallet controls
    (cherry picked from commit 819410eaca58d15551eb3c4f1ddd6b107e54fde9)

---
+++ lib/features/get_paid_settings/public/get_paid_settings_facade.dart
@@ -0,0 +1,5 @@
+export 'package:bb_mobile/features/get_paid_settings/domain/usecases/get_get_paid_wallet_behaviors_usecase.dart'
+    show
+        GetGetPaidWalletBehaviorsUsecase,
+        GetPaidWalletBehavior,
+        GetPaidWalletProduct;
