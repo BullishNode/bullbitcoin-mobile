49d9895b98415956786c750d92970869bd5d3e9d
Francis Pouliot <francis@satoshiportal.com>
2026-07-15 04:14:09 -0600

    feat(invoices): parse exact payer amounts by rail
    (cherry picked from commit 232627f8efd9664006a14078513c58b773c58cab)

--- lib/features/bullnym/domain/bullnym_invoice.dart
+++ lib/features/bullnym/domain/bullnym_invoice.dart
@@ -189,10 +189,13 @@ class BullnymInvoiceStatus {
   final int? paidAtUnix;
   final int? paidAmountSat;
   final String? lightningPr;
+  final int? lightningAmountSat;
   final String? liquidAddress;
+  final int? liquidAmountSat;
   final String? bitcoinAddress;
   final String? bitcoinChainAddress;
   final String? bitcoinChainBip21;
+  final int? bitcoinChainAmountSat;
   final bool acceptBtc;
   final bool acceptLn;
   final bool acceptLiquid;
@@ -215,10 +218,13 @@ class BullnymInvoiceStatus {
     this.paidAtUnix,
     this.paidAmountSat,
     this.lightningPr,
+    this.lightningAmountSat,
     this.liquidAddress,
+    this.liquidAmountSat,
     this.bitcoinAddress,
     this.bitcoinChainAddress,
     this.bitcoinChainBip21,
+    this.bitcoinChainAmountSat,
     required this.acceptBtc,
     required this.acceptLn,
     required this.acceptLiquid,
